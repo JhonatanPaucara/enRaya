5b4a785e2e23cc28da761eb23ccf868d61bcb435
JhonatanPaucara <jhonatan.paucara@gmail.com>
2020-06-04 18:28:11 -0500

    different color for each player

--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'En Raya'),
+      home: MyHomePage(title: 'Tres en Raya'),
     );
   }
 }
@@ -27,34 +28,57 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
   var _marcas = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
   String _playerMark = ' ';
+  var _winner = '';
+  String _msg = 'Game in course';
+  var lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
 
-  void _incrementCounter(position) {
-    setState(() {
-      if (_marcas[position] == ' ') {
-        _counter++;
-        _playerMark = 'O';
-        if (_counter % 2 == 0) {
-          _playerMark = 'X';
+  void _checkWinner(position) {
+    if (_winner != '') {
+      print('ganador' + _winner);
+    } else {
+      print('siguiente');
+      setState(() {
+        if (_marcas[position] == ' ') {
+          _counter++;
+          if (_counter % 2 == 0) {
+            _playerMark = 'X';
+          } else {
+            _playerMark = 'O';
+          }
+          _marcas[position] = _playerMark;
         }
-        _marcas[position] = _playerMark;
+      });
+    }
+    for (var i = 0; i < lines.length; i++) {
+      var option = lines[i];
+      if (_marcas[option[0]] != ' ' &&
+          _marcas[option[0]] == _marcas[option[1]] &&
+          _marcas[option[0]] == _marcas[option[2]]) {
+        setState(() {
+          _winner = _marcas[option[0]];
+          _msg = 'El ganador es: ' + _winner;
+        });
       }
-    });
+    }
   }
 
   void _resetGame() {
     setState(() {
       _counter = 0;
+      _winner = '';
+      _msg = 'Game in course';
       _marcas = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
     });
   }
-
-  /*void _finishGame(){
-    var player1;
-    var player2;
-    for (var i = 0; i < _marcas.length; i++) {
-      
-    }
-  }*/
 
   @override
   Widget build(BuildContext context) {
@@ -62,27 +86,47 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: GridView.count(
-        crossAxisCount: 3,
-        children: List.generate(9, (index) {
-          return Padding(
-            padding: EdgeInsets.all(5.0),
-            child: Container(
-              child: RaisedButton(
-                onPressed: () {
-                  _incrementCounter(index);
-                },
-                child: Text(
-                  _marcas[index],
-                  style: TextStyle(fontSize: 25.0),
-                ),
-                color: Colors.red,
-              ),
-              width: 50.0,
-              height: 50.0,
+      body: Column(
+        children: <Widget>[
+          Center(
+            child: Text(
+              _msg,
+              style: TextStyle(fontSize: 25.0),
             ),
-          );
-        }),
+          ),
+          Expanded(
+            child: GridView.count(
+              crossAxisCount: 3,
+              children: List.generate(9, (index) {
+                return Center(
+                  child: Container(
+                    child: RaisedButton(
+                      onPressed: () {
+                        _checkWinner(index);
+                      },
+                      textColor: Colors.white,
+                      padding: const EdgeInsets.all(0.0),
+                      child: Container(
+                        width: 100.0,
+                        height: 100.0,
+                        color: _marcas[index] == ' '
+                            ? Colors.blueGrey
+                            : _marcas[index] == 'X' ? Colors.red : Colors.blue,
+                        padding: const EdgeInsets.all(0.0),
+                        child: Center(
+                          child: Text(
+                            _marcas[index],
+                            style: TextStyle(fontSize: 25.0),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              }),
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _resetGame,
